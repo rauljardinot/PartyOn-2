7c09851efb9b0daad36484f7d6b5f4a15f9e5c2a
José Carlos Luque Castro <157233434+JoseCarlosLuque@users.noreply.github.com>
2026-05-12 20:28:09 +0200

    [FIX] partyon_presupuestacion : Areglo los cálculos de Margen y precio

--- partyon_presupuestacion/models/partyon_estimate.py
+++ partyon_presupuestacion/models/partyon_estimate.py
@@ -278,12 +278,24 @@ class PartyonEstimate(models.Model):
             if margin_type == 'percent':
                 margin_amt = subtotal * (margin_value / 100.0)
                 suggested = subtotal + margin_amt
+                # En el caso de que se seleccione el porcentaje, entonces será igual que el seleccionado.
+                print("Hola mi loco " + str(estimate.margin_percent) )
+                estimate.margin_percent = margin_value / 100
             elif margin_type == 'amount':
                 margin_amt = margin_value
                 suggested = subtotal + margin_amt
+                # En el caso de que el margen sea un valor absoluto, se hace el cálculo para verlo
+                estimate.margin_percent = (
+                    ((margin_amt / subtotal * 100.0) / 100) if subtotal else 0.0
+                )
+
             elif margin_type == 'manual':
                 suggested = estimate.manual_sale_price
                 margin_amt = suggested - subtotal
+
+                estimate.margin_percent = (
+                    ((margin_amt / subtotal * 100.0)/100) if subtotal else 0.0
+                )
             else:
                 margin_amt = 0.0
                 suggested = subtotal
@@ -291,9 +303,11 @@ class PartyonEstimate(models.Model):
             estimate.suggested_sale_price = suggested
             estimate.sale_price = suggested
             estimate.margin_amount = margin_amt
-            estimate.margin_percent = (
-                (margin_amt / subtotal * 100.0) if subtotal else 0.0
-            )
+
+            # El porcentaje de margen cambiará en función de lo seleccionado.
+            # estimate.margin_percent = (
+            #     (margin_amt / subtotal * 100.0) if subtotal else 0.0
+            # )
 
     # -------------------------------------------------------------------------
     # CRUD
@@ -451,7 +465,7 @@ class PartyonEstimate(models.Model):
     def action_apply_estimate(self):
         for rec in self:
             if rec.template_id:
-                rec.line_ids.unlink() # TODO: Preguntar si es mejor hacer un False.
+                rec.line_ids.unlink()  # TODO: Preguntar si es mejor hacer un False.
                 rec.line_ids = rec.template_id.partyon_estimate_lines_ids
                 # TODO: Pregutnar si se puede hacer así para que no pase referencia en memoria.
 
