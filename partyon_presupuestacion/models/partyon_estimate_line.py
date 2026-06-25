de8b83896b1f077076f9f39f1c7c181ca0d73508
Raul Rolando Jardinot Gonzalez <49165764+rauljardinot@users.noreply.github.com>
2026-06-24 18:42:54 -0700

    [REF] partyon_presupuestacion: refactor and cleaning code

--- partyon_presupuestacion/models/partyon_estimate_line.py
+++ partyon_presupuestacion/models/partyon_estimate_line.py
@@ -60,16 +60,20 @@ class PartyonEstimateLine(models.Model):
         store=True,
     )
     # -------------------------------------------------------------------------
+    # Precio de Venta Unitario
+    # -------------------------------------------------------------------------
+    lst_price = fields.Float('Precio de Venta', currency_field='currency_id', help='Precio unitario del material por pieza.')
+    # -------------------------------------------------------------------------
     # COSTES UNITARIOS
     # -------------------------------------------------------------------------
     cost_product_unit = fields.Monetary(
-        string='Coste material (unitario)',
+        string='Coste/Unitario',
         currency_field='currency_id',
         help='Coste unitario del material por pieza.',
     )
     # Coste unitario real contando el beneficio.
     cost_unit_real = fields.Monetary(
-        string='Coste Real Ud.',
+        string='Coste/Unitario',
         currency_field='currency_id',
         # compute='_compute_cost_unit_real',
         help='Coste unitario del material por pieza.',
@@ -79,7 +83,13 @@ class PartyonEstimateLine(models.Model):
     # -------------------------------------------------------------------------
 
     line_subtotal = fields.Monetary(
-        string='Total línea',
+        string='Precio Total',
+        compute='_compute_subtotals',
+        store=True,
+        currency_field='currency_id',
+    )
+    line_cost_subtotal = fields.Monetary(
+        string='Coste Total',
         compute='_compute_subtotals',
         store=True,
         currency_field='currency_id',
@@ -136,49 +146,9 @@ class PartyonEstimateLine(models.Model):
     @api.depends('quantity', 'cost_product_unit')
     def _compute_subtotals(self):
         for line in self:
-            qty = line.quantity
-            line.line_subtotal = line.cost_unit_real * qty
-
-    # # Coste real por unidad
-    # @api.depends('product_id')
-    # def _compute_cost_unit_real(self):
-    #     for record in self:
-    #         if record.product_id:
-    #             record.cost_unit_real = record.cost_product_unit * (1 + record.product_margin_percentage / 100)
-
-    # Precio real de la línea con el beneficio.
-    # @api.depends(
-    #     'line_subtotal', 'quantity',
-    #     'line_margin_type', 'line_margin_value',
-    #     'estimate_id.margin_type', 'estimate_id.margin_value',
-    # )
-    # # def _compute_sale_price_unit(self):
-    # #     for line in self:
-    # #         subtotal = line.line_subtotal
-    # #         qty = line.quantity or 1.0
-    # #
-    # #         if line.line_margin_type == 'percent':
-    # #             margin_val = line.line_margin_value
-    # #             price = subtotal * (1 + margin_val / 100.0)
-    # #         elif line.line_margin_type == 'amount':
-    # #             price = subtotal + line.line_margin_value
-    # #         else:
-    # #             # 'global' — usa margen del presupuesto padre
-    # #             est = line.estimate_id
-    # #             if est.margin_type == 'percent':
-    # #                 price = subtotal * (1 + (est.margin_value / 100.0))
-    # #             elif est.margin_type == 'amount':
-    # #                 # Distribuir proporcionalmente el importe fijo
-    # #                 total_cost = est.subtotal_cost or 1.0
-    # #                 price = subtotal + (est.margin_value * subtotal / total_cost)
-    # #             else:
-    # #                 # Manual — distribuir proporcionalmente
-    # #                 total_cost = est.subtotal_cost or 1.0
-    # #                 if est.manual_sale_price:
-    # #                     price = est.manual_sale_price * subtotal / total_cost
-    # #                 else:
-    # #                     price = subtotal
-    # #         line.sale_price_unit = price / qty if qty else price
+            # qty = line.quantity
+            line.line_subtotal = line.lst_price * line.quantity
+            line.line_cost_subtotal = line.cost_product_unit * line.quantity
 
     # -------------------------------------------------------------------------
     # ONCHANGE
@@ -189,7 +159,8 @@ class PartyonEstimateLine(models.Model):
             if self.product_id:
                 record.name = record.product_id.display_name
                 record.uom_id = record.product_id.uom_id
-                record.cost_product_unit = record.product_id.list_price or 0.0
+                record.cost_product_unit = record.product_id.standard_price
                 record.product_margin_percentage = record.product_id.margin_percentage or 0.0
+                record.lst_price = record.product_id.lst_price
 
-                record.cost_unit_real = record.cost_product_unit * (1 + record.product_margin_percentage / 100) or 0.0
+                # record.cost_unit_real = record.product_id.standard_price
