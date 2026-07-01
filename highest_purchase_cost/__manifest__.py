b9a29a2421b9b96b2ab0cf188af8a9d31bd26a07
José Carlos Luque Castro <157233434+JoseCarlosLuque@users.noreply.github.com>
2026-07-01 20:03:56 +0200

    fix module

--- highest_purchase_cost/__manifest__.py
+++ highest_purchase_cost/__manifest__.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Mayor Precio de Compra',
+    'name': 'Highest Purchase Cost',
     'version': '19.0.1.0.0',
     'category': 'Inventory/Inventory',
-    'summary': 'Metodo de coste basado en el mayor precio de compra',
+    'summary': 'This module allows you yo select the highest purchase cost for your product.',
     'description': """
-        Anade un metodo de coste que mantiene el coste del producto en el
-        mayor precio unitario recibido en compras.
+        This module allows you yo select the highest purchase cost for your product.
     """,
     'author': 'Raul Rolando Jardinot Gonzalez / Jose Carlos Luque Castro',
-    'license': 'LGPL-3',
+    'license': 'OPL-1',
     'depends': ['stock_account', 'purchase_stock', ],
     'installable': True,
     'application': False,
     'auto_install': False,
+    'price': 250,
+    'currency': 'USD',
 }
