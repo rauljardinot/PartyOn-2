6587ad97198996efd556481a3e1df02fb80ca8ea
José Carlos Luque Castro <157233434+JoseCarlosLuque@users.noreply.github.com>
2026-07-24 17:33:04 +0200

    [IMP] ss_hide_menus: Improve the group control in the user form

--- ss_hide_menus/__manifest__.py
+++ ss_hide_menus/__manifest__.py
@@ -10,17 +10,15 @@ Module to hide the menus to certain users
     'author': "Simplicia Solutions",
     'website': "https://www.simpliciasolutions.com",
 
-
     'category': 'Uncategorized',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','mail','calendar','website_links','utm'],
+    'depends': ['base', 'mail', 'calendar', 'website_links', 'utm'],
 
     # always loaded
     'data': [
         'security/security.xml',
-        'views/menu.xml'
+        'views/menu.xml',
     ]
 }
-
